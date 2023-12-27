69df66f6357b251a08b21d458a0aff0bd0a9eade
Manu Quiroga <mquiroga@itpatagonia.com>
2023-12-27 09:48:04 -0300

    Adds AutoMapper and CreateAuthorDto

---
+++ BookAPI/Properties/Config/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BookAPI.Data;
+using BookAPI.Models.Author;
+
+namespace BookAPI.Properties.Config
+{
+    public class MapperConfig: Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<CreateAuthorDto, Author>().ReverseMap();
+        }
+    }
+}
